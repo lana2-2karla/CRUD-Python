356808c9b2255bff4a48ab5748c41e785a57808f
lana2-2karla <allanakarla1702@gmail.com>
2023-01-11 11:51:59 -0300

    :sparkles: feat: database connection

--- crud.py
+++ crud.py
@@ -1,1 +1,15 @@
-print("hello word")
+import mysql.connector
+
+connection = mysql.connector.connect(
+    host='localhost',
+    user='root',
+    password='12345678',
+    database='bdSales',
+)
+
+cursor = connection.cursor()
+
+#CRUD aqui
+
+cursor.close()
+connection.close()
